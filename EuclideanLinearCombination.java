f9a59d191055adbba00a8c6c749346c5486d6fea
Arhum Khan <37052066+arhumkhan@users.noreply.github.com>
2018-10-20 18:28:14 -0400

    Added some comments and better variable names

--- EuclideanLinearCombination.java
+++ EuclideanLinearCombination.java
@@ -8,26 +8,25 @@ public class EuclideanLinearCombination
 		int[] numA = new int[10];
 		int[] bScalar = new int[10];
 		int[] numB = new int[10];
+		//can only do ten steps
 		
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Enter first number:");
 		numA[0] = kb.nextInt();
 		System.out.println("Enter second number:");
 		numB[0] = kb.nextInt();
-		
 		if(numB[0] > numA[0])
 		{
 			int x = numB[0];
 			numB[0] = numA[0];
 			numA[0] = x;
 		}
-		
 		bScalar[0] = 1;
 		
-		int i = 1;
+		System.out.println("Obtaining the GCD with Euclidean Algorithm:");
+		int steps = 1;
 		int diff = numA[0] % numB[0];
 		bScalar[0] = numA[0] / numB[0];
-		
 		
 		System.out.println(diff + " = " 
 				+ aScalar[0] + "(" + numA[0] + ")" + " - " 
@@ -35,51 +34,50 @@ public class EuclideanLinearCombination
 		
 		while(diff > 0)
 		{
-			numA[i] = numB[i - 1];
-			numB[i] = diff;
-			diff = numA[i] % numB[i];
-			bScalar[i] = numA[i] / numB[i];
+			numA[steps] = numB[steps - 1];
+			numB[steps] = diff;
+			diff = numA[steps] % numB[steps];
+			bScalar[steps] = numA[steps] / numB[steps];
 			System.out.println(diff + " = " 
-					+ aScalar[i] + "(" + numA[i] + ")" + " - " 
-					+ bScalar[i] + "(" + numB[i] + ")");
-			i++;
+					+ aScalar[steps] + "(" + numA[steps] + ")" + " - " 
+					+ bScalar[steps] + "(" + numB[steps] + ")");
+			steps++;
 		}
 		//this iterates past our gcd value twice so we gotta roll back our values... twice
-		diff = numA[i-2] % numB[i-2];
-		i = i - 2;
+		diff = numA[steps - 2] % numB[steps - 2];
+		steps = steps - 2;
 		
 		
-		System.out.println(" = = = = = =");
-		
+		System.out.println("Working backwards from the end to the beginning:");
 		//decrement down from i
-		while(i > 0)
+		while(steps > 0)
 		{
-			System.out.println("combining iteration: " + i);
-			aScalar[i] = aScalar[i] + (bScalar[i] * bScalar[i - 1]);
-			bScalar[i] = (aScalar[i - 1]) * bScalar[i];
-			numB[i] = numA[i - 1];
+			System.out.println("Iteration #" + steps);
+			aScalar[steps] = aScalar[steps] + (bScalar[steps] * bScalar[steps - 1]);
+			bScalar[steps] = (aScalar[steps - 1]) * bScalar[steps];
+			numB[steps] = numA[steps - 1];
 			System.out.println(diff + " = " 
-					+ aScalar[i] + "(" + numA[i] + ")" + " - " 
-					+ bScalar[i] + "(" + numB[i] + ")");
+					+ aScalar[steps] + "(" + numA[steps] + ")" + " - " 
+					+ bScalar[steps] + "(" + numB[steps] + ")");
 			
 			//switching places for consistency lol
 			//bigger number to the left, num replaced to the right
 			int placeHolder = 0;
-			placeHolder = aScalar[i];
-			aScalar[i] = bScalar[i];
-			bScalar[i] = placeHolder;
-			placeHolder = numA[i];
-			numA[i] = numB[i];
-			numB[i] = placeHolder;
+			placeHolder = aScalar[steps];
+			aScalar[steps] = bScalar[steps];
+			bScalar[steps] = placeHolder;
+			placeHolder = numA[steps];
+			numA[steps] = numB[steps];
+			numB[steps] = placeHolder;
 			
-			//annnnd the whole new equation into i - 1
+			//annnnd the whole new equation into steps - 1
 			//probably a better way to do this
-			aScalar[i-1] = aScalar[i];
-			numA[i-1] = numA[i];
-			bScalar[i-1] = bScalar[i];
-			numB[i-1] = numB[i];
+			aScalar[steps-1] = aScalar[steps];
+			numA[steps-1] = numA[steps];
+			bScalar[steps-1] = bScalar[steps];
+			numB[steps-1] = numB[steps];
 			
-			i--;
+			steps--;
 		}
 		
 		System.out.println();
@@ -87,6 +85,5 @@ public class EuclideanLinearCombination
 		System.out.println(diff + " = " 
 				+ aScalar[0] + "(" + numA[0] + ") - " 
 				+ bScalar[0] + "(" + numB[0] + ")");
-		
 	}
 }
